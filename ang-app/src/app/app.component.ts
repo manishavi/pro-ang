302e5a678263a94ed637f2523736e5ffa5605352
manishavi <manishalal972@gmail.com>
2018-10-03 11:59:01 -0500

    using inline template

--- ang-app/src/app/app.component.ts
+++ ang-app/src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>Inline Template</h1>
+  `,
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
