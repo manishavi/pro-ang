d7be60e43c1329f2da295d8ec8535957fdb62d57
manishavi <manishalal972@gmail.com>
2018-10-03 12:00:52 -0500

    reverted back

--- ang-app/src/app/app.component.ts
+++ ang-app/src/app/app.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Inline Template</h1>
-  `,
-  // templateUrl: './app.component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
